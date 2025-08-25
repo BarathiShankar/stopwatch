5add1e389b7ca194bb62452b8fffd7d9d805043e
T S BHUVANESHWAR <bhuvaneshwarts1@gmail.com>
2025-08-25 14:55:46 +0530

    Create j.js

---
+++ j.js
@@ -0,0 +1,37 @@
+let display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+function start()
+{
+    if(!false)
+    {
+        startTime = Date.now()-elapsedTime;
+        timer = setInterval(update,10);
+        isRunning = false;
+    }
+}
+function reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00"
+}
+function stop(){
+    clearInterval(timer);
+    isRunning = false;
+    //elapsedTime = Date.now() - startTime;
+}
+function update(){
+    elapsedTime = Date.now() - startTime;
+    let hour = Math.floor(elapsedTime/(1000*60*60));
+    let minute = Math.floor((elapsedTime/((1000*60 ))%60));
+    let second = Math.floor((elapsedTime/1000)%60 );
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+    hour  = hour<10?"0"+hour:hour;
+    minute = minute<10?"0"+minute:minute;
+    second = second<10?"0"+second:second;
+    display.textContent = `${hour}:${minute}:${second}:${milliseconds}`;
+}
